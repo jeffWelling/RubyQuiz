e2297a8deb0cbd6aedac9c256fb1500181f6fe54
Jeff Welling <Jeff.Welling@Gmail.com>
2010-08-19 05:49:44 -0700

    Added accessor for neighbors and added a Maze class which creates a board

--- quiz31/maze.rb
+++ quiz31/maze.rb
@@ -1,10 +1,11 @@
-
 class Cell
 	def initialize borders=nil, walls=nil
 		@borders= ( borders.nil? ? 0 : borders )
 		@walls= (walls.nil? ? 3 : walls)
 		@neighbors={:north=>nil, :east=>nil, :south=>nil, :west=>nil}
 	end
+
+	attr_accessor :neighbors
 
 	def set_north_wall
 		@walls+=1 unless @walls.odd?
@@ -50,4 +51,34 @@ class Cell
 		[@borders,@walls]
 	end
 
+end
+
+class Maze
+	def initialize length, width
+		raise "length and width must be fixnums" unless length.class==Fixnum and width.class==Fixnum
+		#to compensate for starting from zero
+		length-=1
+		width-=1
+
+		first_cell=nil
+		@board=[[]]
+		(0..length).each {|l|
+			@board[l] ||=[]
+			(0..width).each{|w|
+				@board[l][w]||=nil
+				@board[l][w]=cell=Cell.new
+				next if l==0 and w==0
+				if l==0
+					#don't link to the one above, it doesn't exist
+					@board[l][w-1].add_neighbor :east, cell
+				elsif w==0
+					@board[l-1][w].add_neighbor :south, cell
+				else
+					@board[l-1][w].add_neighbor :south, cell
+					@board[l][w-1].add_neighbor :east, cell
+				end
+			}
+		}
+		@board
+	end
 end
