c917464e46ebc488dd983994f55129709ef79b76
Jeff Welling <Jeff.Welling@Gmail.com>
2010-08-19 04:14:52 -0700

    Fixed some things

--- quiz31/maze.rb
+++ quiz31/maze.rb
@@ -6,22 +6,35 @@ class Cell
 	end
 
 	def set_north_wall
-		@walls+=1 unless @walls%2
+		@walls+=1 unless @walls.odd?
 	end
 
 	def unset_north_wall
-		@walls
+		@walls-=1 if @walls.odd?
+	end
 
-	def set_north_boarder
-		@borders+=1 unless @boardesr%2
+	def set_north_border
+		@borders+=1 unless @borders.odd?
+	end
+
+	def unset_north_border
+		@borders-=1 if @borders.odd?
 	end
 
 	def set_west_wall
-		@walls+=2 unless @walls/2
+		@walls+=2 unless @walls.even?
 	end
 
-	def set_west_boarder
-		@borders+=2 unless @borders/2
+	def unset_west_wall
+		@walls-=2 if @walls.even?
+	end
+
+	def set_west_border
+		@borders+=2 unless @borders.even?
+	end
+
+	def unset_west_border
+		@borders-=2 if @borders.even?
 	end
 
 	def dump
